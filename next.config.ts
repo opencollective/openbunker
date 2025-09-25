779701f8051a647d93ac4ea7ccd94f619c698455
spotchi <quentin.laurent93@gmail.com>
2025-09-25 14:21:39 +0200

    fix: ignore supabase functions

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,26 @@ const nextConfig: NextConfig = {
       'utf-8-validate': 'commonjs utf-8-validate',
       bufferutil: 'commonjs bufferutil',
     });
+
+    // Exclude Supabase functions from webpack compilation
+    config.module.rules.push({
+      test: /supabase\/functions\/.*\.ts$/,
+      use: 'ignore-loader',
+    });
+
     return config;
   },
   images: {
     remotePatterns: [new URL('https://cdn.discordapp.com/avatars/**')],
+  },
+  // Exclude Supabase folder from TypeScript compilation
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  // Exclude Supabase folder from ESLint
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: ['src', 'app'],
   },
 };
 
